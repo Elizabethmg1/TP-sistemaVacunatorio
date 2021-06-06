5dfaf4dc978b448521de39eb783c549bf6c4f7c2
Micaela <mica.gmnz@gmail.com>
2021-06-05 23:45:06 -0300

    agrego TAD Turnos, modifico metodos en CentroVacunacion y en Persona

--- CentroVacunacion.java
+++ CentroVacunacion.java
@@ -1,6 +1,8 @@
 package centroVacunacion;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class CentroVacunacion {
 	private String nombre;
@@ -10,42 +12,61 @@ public class CentroVacunacion {
 	private Vacunatorio vacunatorio;
 
 		public CentroVacunacion(String nombre,int capacidadDiaria) {
-			
-		this.nombre=nombre;
-		this.capacidadDiaria=capacidadDiaria;
-		HashMap<Fecha,Turnos> aux1=new HashMap<Fecha,Turnos>();
-		this.fechaConTurnos=aux1;
-		HashSet<Persona> aux2=new HashSet<Persona>();
-		this.listaDeEspera=aux2;
-		Vacunatorio aux3=new Vacunatorio();
-		this.vacunatorio=aux3;
+			if(capacidadDiaria<=0) {
+				throw new RuntimeException("la capacidad ingresada debe ser un valor positivo");
+			}
+			else {
+				this.nombre=nombre;
+				this.capacidadDiaria=capacidadDiaria;
+				HashMap<Fecha,Turnos> aux1=new HashMap<Fecha,Turnos>();
+				this.fechaConTurnos=aux1;
+				HashSet<Persona> aux2=new HashSet<Persona>();
+				this.listaDeEspera=aux2;
+				Vacunatorio aux3=new Vacunatorio();
+				this.vacunatorio=aux3;
+			}
 	}
 	
-		public boolean ingresarVacunas(String nombre, int cantidad, Fecha fecha) {
-		return this.vacunatorio.ingresanVacunas(nombre,cantidad,fecha);
+		public void ingresarVacunas(String nombre, int cantidad, Fecha fecha) {
+			if(!nombre.equals("Sputnik") && !nombre.equals("Astrazeneca") && !nombre.equals("Pfizer") && !nombre.equals("Moderna") && !nombre.equals("Sinopharm")) {
+				throw new RuntimeException("la vacuna ingresada no es valida");
+			}
+			if(cantidad<=0) {
+				throw new RuntimeException("la cantidad no es valida");
+			}
+			this.vacunatorio.ingresanVacunas(nombre,cantidad,fecha);
 	}
 	
-		public boolean inscribirPersona(int dni,Fecha fecha, boolean trabajadorEsencial, boolean enfermedadPreexistente) {
-		Persona aux=new Persona(dni,fecha,trabajadorEsencial,enfermedadPreexistente);
-		if(!this.listaDeEspera.contains(aux)) {
-			this.listaDeEspera.add(aux);
-			return true;
-		}
-		else {
-			return false;
-		}	
-	}
-	
-		public boolean generarTurnos(Fecha fechaInicial) {
-			Turnos turnos=new Turnos(this.capacidadDiaria);
-			if (!this.fechaConTurnos.containsKey(fechaInicial)) {
-				this.fechaConTurnos.put(fechaInicial, turnos);
+		public void inscribirPersona(Integer dni,Fecha fechaNac, boolean trabajadorEsencial, boolean enfermedadPreexistente) {
+			Persona aux=new Persona(dni,fechaNac,trabajadorEsencial,enfermedadPreexistente);
+			if(!this.listaDeEspera.contains(aux) && fechaNac.diferenciaAnios(Fecha.hoy(), fechaNac)>=18) {
+				this.listaDeEspera.add(aux);
 			}
 		}
+	
+		public void generarTurnos(Fecha fechaInicial) {
+//			Set<Fecha> fechas=this.fechaConTurnos.keySet();
+//			for(Fecha f:fechas) {
+//				if(f.anterior(Fecha.hoy())) {
+//					Turnos turnos=this.fechaConTurnos.get(f);
+//				}
+//			}
+			Set<Fecha> fechas=this.fechaConTurnos.keySet();
+			
+			
+//			Turnos turnos=new Turnos(this.capacidadDiaria);
+//			if (!this.fechaConTurnos.containsKey(fechaInicial)) {
+//				this.fechaConTurnos.put(fechaInicial, turnos);
+//			}
+		}
 		
 		
-		public HashSet<Persona> listaDeEspera(){
-			return this.listaDeEspera;
+		public HashSet<Integer> listaDeEspera(){
+			HashSet<Integer> dnis=new HashSet<Integer>();
+			for(Persona persona:this.listaDeEspera) {
+				dnis.add(persona.darDni());
+			}
+			return dnis;
 		}
 		
 		public int vacunasDisponibles() { //todas las vacunas
@@ -56,21 +77,31 @@ public class CentroVacunacion {
 			this.vacunatorio.darCantidadDeVacunas(vacuna);
 		}
 		
-		public HashSet<Persona> turnosConFecha(Fecha fecha){  //
+		public Set<Persona> turnosConFecha(Fecha fecha){  //
 			if(this.fechaConTurnos.containsKey(fecha)) {
 				return this.fechaConTurnos.get(fecha).darListaPersona();
 			}
 		}
-		
-		public HashMap<Persona,String> reporteVacunacion() {
-			return null; //completar
+
+		//USO FOR EACH
+		public HashMap<Integer,String> reporteVacunacion() {
+			Set<Fecha> fechas=this.fechaConTurnos.keySet(); //hago un set de fechas
+			HashMap<Integer,String> reporte=new HashMap<Integer,String>(); //hago un map reporte donde guardar los vacunados
+			for(Fecha f:fechas) { //itero las fechas
+				HashMap<Integer,String> reporte1=this.fechaConTurnos.get(f).darListaPersonasVacunadas(); //obtengo las personas que fueron vacunadas
+				Set<Integer> dnis=reporte1.keySet();
+				for(Integer i:dnis) { //itero las personas que fueron vacunadas
+					reporte.put(i, reporte1.get(i)); //las agrego al reporte a devolver.
+				}
+			}
+			return reporte;
 	}
 		
-		public HashMap<Persona,String> reporteVacunasVencidas(){
+		public HashMap<String,Integer> reporteVacunasVencidas(){
 			return null; //completar
 		}
 		
-		public boolean vacunarInscripto(int dni, Fecha fecha) {
-			return false;
+		public void vacunarInscripto(int dni, Fecha fecha) {
+			//completaar
 		}
 }
